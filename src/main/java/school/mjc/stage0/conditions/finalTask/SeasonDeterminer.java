500eb6c0fb331ae200f8cc1d7652c34dda08099f
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-11-01 14:51:00 +0500

    bugs fixed

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        String season = switch (monthNumber) {
+            case (1), (2), (12) -> "winter";
+            case (3), (4), (5) -> "spring";
+            case (6), (7), (8) -> "summer";
+            case (9), (10), (11) -> "autumn";
+            default -> "Wrong month number";
+        };
+
+        System.out.println(season);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        boolean bool1 = (firstSide + secondSide > thirdSide);
+        boolean bool2 = (secondSide + thirdSide > firstSide);
+        boolean bool3 = (thirdSide + firstSide > secondSide);
+
+        if (bool1 && bool2 && bool3) {
+            System.out.println("this is a valid triangle");
+        }
+        else {
+            System.out.println("it's not a triangle");
+        }
 
     }
 }
