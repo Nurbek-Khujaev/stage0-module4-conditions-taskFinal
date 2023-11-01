9650f33651e99bd92d2fcfced181f6e19797361a
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-11-02 00:48:03 +0500

    bugs fixed

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -7,9 +7,9 @@ public class CoordinatePane {
         } else if (x > 0 && y > 0) {
             System.out.println("first");
         } else if (x < 0 && y > 0) {
-            System.out.println("first");
+            System.out.println("second");
         } else if (x < 0 && y < 0) {
-            System.out.println("first");
+            System.out.println("third");
         } else {
             System.out.println("fourth");
         }
